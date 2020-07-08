34728bbe14b772f78ede05de6de0285adce9546a
Rupak Dey <48596997+deyRupak@users.noreply.github.com>
2020-07-09 00:43:08 +0530

    Add Breakpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from "react";
 import "./styles.css";
 
+import {Grid} from '@material-ui/core'
+import { Breakpoint, BreakpointProvider } from 'react-socks';
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-    </div>
+    <>
+    {/* <div className="App">
+    </div> */}
+    
+
+    <BreakpointProvider>
+            <Breakpoint small down>
+                <Grid container
+                direction="column"
+                justify="center"
+                alignItems="center">
+                    <Grid item xs={12} className="grid-style1">code<br />sandbox</Grid>
+                    <Grid item xs={12} className="grid-style">x</Grid>
+                    <Grid item xs={12} className="grid-style">c</Grid>
+                </Grid>
+            </Breakpoint>
+
+
+            <Breakpoint large up>
+            <Grid container
+            justify="center"
+            alignItems="center">
+                  <Grid item xs={4} className="grid-style1">code<br />sandbox</Grid>
+                  <Grid item xs={4} className="grid-style">x</Grid>
+                  <Grid item xs={4} className="grid-style">c</Grid>
+            </Grid>
+            </Breakpoint>
+    </BreakpointProvider>
+    </>
   );
 }
