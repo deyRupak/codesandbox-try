f93eb87b54da75ed15ec8c151398be38ef3aa3be
Rupak Dey <48596997+deyRupak@users.noreply.github.com>
2020-07-10 00:39:58 +0530

    Add Grid Content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 
-import {Grid} from '@material-ui/core'
+import {Grid, Paper} from '@material-ui/core'
 import { Breakpoint, BreakpointProvider } from 'react-socks';
 
 export default function App() {
@@ -18,7 +18,18 @@ export default function App() {
                 justify="center"
                 alignItems="center">
                     <Grid item xs={12} className="grid-style1">code<br />sandbox</Grid>
-                    <Grid item xs={12} className="grid-style">x</Grid>
+                    <Grid item xs={10} style={{textAlign: 'justify', margin: 6}}>
+                      {/* <Paper style={{width: '90%', margin: 10}}> */}
+                        This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the 'body' tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+                    {/* </Paper> */}
+                    </Grid>
                     <Grid item xs={12} className="grid-style">c</Grid>
                 </Grid>
             </Breakpoint>
@@ -29,8 +40,26 @@ export default function App() {
             justify="center"
             alignItems="center">
                   <Grid item xs={4} className="grid-style1">code<br />sandbox</Grid>
-                  <Grid item xs={4} className="grid-style">x</Grid>
-                  <Grid item xs={4} className="grid-style">c</Grid>
+                  <Grid item xs={4} style={{textAlign: 'justify', padding: 32}}>
+                  This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the 'body' tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+                  </Grid>
+                  <Grid item xs={4} style={{textAlign: 'justify', padding: 32}}>
+                  This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the 'body' tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+                  </Grid>
             </Grid>
             </Breakpoint>
     </BreakpointProvider>
